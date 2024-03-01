a18e1b2bc2aa5c765206ced2cd811efd08ebcb84
wwwww <uuuyuyuy@email.com>
2024-03-01 17:01:55 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asjkdh, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
